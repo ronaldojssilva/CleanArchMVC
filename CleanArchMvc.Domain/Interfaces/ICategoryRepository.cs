8990dee9ab801f27106348fc8129029097ee62b8
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-15 15:04:09 -0400

    Criando Interfaces Repositories

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    internal interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    internal interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
