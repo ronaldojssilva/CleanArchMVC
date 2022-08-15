4544b68ee5598edacb75c4898c48a4d5155e4f14
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-15 14:21:07 -0400

    Domain Rich Enriquecendo Modelo

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,29 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    internal class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        public int Id { get; private set; }
+        public string Name { get; private set; }
         public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characteres");
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
     }
 }
