796738794a3e62fb8cec65e33ce56337f735efce
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-16 09:39:37 -0400

    Testes Unidade Product

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -37,12 +37,12 @@ namespace CleanArchMvc.Domain.Entities
         private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
-            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characteres");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
             DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description.Description is required");
             DomainExceptionValidation.When(description.Length < 5, "Invalid description, too short, minimum 5 characteres");
             DomainExceptionValidation.When(price < 0, "Invalid price value");
             DomainExceptionValidation.When(stock < 0, "Invalid stock value");
-            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, minimum 250 characteres");
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximum 250 characters");
 
             Name = name;
             Description = description;
