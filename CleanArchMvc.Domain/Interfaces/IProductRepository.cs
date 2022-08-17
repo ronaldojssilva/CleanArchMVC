eb466dbc9082057fb1eab11f2ec813d076b3981e
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-17 00:24:33 -0400

    Consideracoes Implementacao CQRS

--- CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -11,7 +11,7 @@ namespace CleanArchMvc.Domain.Interfaces
     {
         Task<IEnumerable<Product>> GetProductsAsync();
         Task<Product> GetByIdAsync(int? id);
-        Task<Product> GetProductCategoryAsync(int? id);
+        //Task<Product> GetProductCategoryAsync(int? id);
         Task<Product> CreateAsync(Product category);
         Task<Product> UpdateAsync(Product category);
         Task<Product> RemoveAsync(Product category);
