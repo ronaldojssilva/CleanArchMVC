0bf78f40651e569c6d6129f8cee9ae041bc6ccca
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-16 21:57:13 -0400

    Criando Controller View Products

---
+++ CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productAppService)
+        {
+            _productService = productAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
