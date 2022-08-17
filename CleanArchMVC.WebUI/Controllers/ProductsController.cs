f9084c2dc5657e345c626fe32559a56b9ea6ff66
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-17 16:09:37 -0400

    Create Product

--- CleanArchMVC.WebUI/Controllers/ProductsController.cs
+++ CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -1,15 +1,20 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMVC.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productAppService)
+
+        public ProductsController(IProductService productAppService, ICategoryService categoryService)
         {
             _productService = productAppService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -17,6 +22,26 @@ namespace CleanArchMVC.WebUI.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+            new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
         }
     }
 }
