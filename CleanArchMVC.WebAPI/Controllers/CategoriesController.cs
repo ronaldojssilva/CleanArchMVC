9a5cd3b32234f9142cab0c819e5794c2b0e6d41b
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-18 15:26:21 -0400

    Categories GET

--- CleanArchMVC.WebAPI/Controllers/CategoriesController.cs
+++ CleanArchMVC.WebAPI/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMVC.WebAPI.Controllers
@@ -7,5 +9,22 @@ namespace CleanArchMVC.WebAPI.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+            return Ok(categories);
+        }
+
     }
 }
