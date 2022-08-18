d65923fa9c53af4d9527f1cbef4aa9f630dbc226
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-18 11:44:40 -0400

    ViewModel

---
+++ CleanArchMVC.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+    }
+}
