2d29c0bc87bc4661ba5b0394b34a535f2732f76b
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-17 22:31:13 -0400

    Seguranca Interfaces Domain

---
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Account
+{
+    internal interface IAuthenticate
+    {
+        Task<bool> Authenticate(string email, string password);
+
+        Task<bool> RegisterUser(string email, string password);
+
+        Task Logout();
+    }
+}
