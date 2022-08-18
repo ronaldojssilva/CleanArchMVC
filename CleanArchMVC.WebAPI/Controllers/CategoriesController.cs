59ce120fc3a43b8a4c4b3a723291a9e1553405a7
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-18 15:10:51 -0400

    Cria Categories

---
+++ CleanArchMVC.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMVC.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
