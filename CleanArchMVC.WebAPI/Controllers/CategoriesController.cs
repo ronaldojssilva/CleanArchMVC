d5a9cd56daf7be614fe563e459836330bfbceec7
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-19 11:26:56 -0400

    Categories Post

--- CleanArchMVC.WebAPI/Controllers/CategoriesController.cs
+++ CleanArchMVC.WebAPI/Controllers/CategoriesController.cs
@@ -26,7 +26,7 @@ namespace CleanArchMVC.WebAPI.Controllers
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCategory")]
         public async Task<ActionResult<CategoryDTO>> Get(int id)
         {
             var category = await _categoryService.GetById(id);
@@ -35,6 +35,18 @@ namespace CleanArchMVC.WebAPI.Controllers
                 return NotFound("Category not found");
             }
             return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+        {
+            if (categoryDto == null)
+                return BadRequest("Invalid Data");
+
+            await _categoryService.Add(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.Id },
+                categoryDto);
         }
     }
 }
