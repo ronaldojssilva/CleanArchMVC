e4a2c2835765a01598ba48a724ee7c2c42ef54d4
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-26 12:33:21 -0400

    Products Controller

--- CleanArchMVC.Application/DTOs/ProductDTO.cs
+++ CleanArchMVC.Application/DTOs/ProductDTO.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace CleanArchMvc.Application.DTOs
 {
@@ -37,7 +38,8 @@ namespace CleanArchMvc.Application.DTOs
         [MaxLength(250)]
         [DisplayName("Product Image")]
         public string Image { get; set; }
-        
+
+        [JsonIgnore]
         public Category? Category { get; set; }
 
         [DisplayName("Categories")]
